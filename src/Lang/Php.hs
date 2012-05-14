5124d684fee8e9e81a73b9c2a14eee18a694c6b6
Tomasz Dudziak <tomasz.dudziak@gmail.com>
2012-05-14 15:07:01 +0200

    Get rid of most of the lex-pass stuff.

--- src/Lang/Php.hs
+++ src/Lang/Php.hs
@@ -1,11 +1,9 @@
 module Lang.Php (
   module Lang.Php.Ast,
-  module Lang.Php.Transf,
   ftype
   ) where
 
 import Lang.Php.Ast
-import Lang.Php.Transf
 
 ftype = ["php"]
 
--- src/ghci.hs
+++
@@ -1,6 +0,0 @@
-{- for easy interactive testing, start ghci in src/ and then do  :l ghci.hs -}
-import Ast
-import Control.Applicative
-import Data.Tok
-import LexPassUtil
-import Text.Parsec
