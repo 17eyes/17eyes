45a25c4134a8b9eae3557979dca130395f75aaff
Tomasz Dudziak <tomasz.dudziak@gmail.com>
2012-05-14 17:05:06 +0200

    Set default input encoding to latin2.

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,11 @@
 -- Dummy program that parses standard input and dumps the AST.
 
+import System.IO
 import Lang.Php.Ast
 
 main :: IO ()
 main = do
+    hSetEncoding stdin latin1
     input <- getContents
     case runParser parse () "<stdin>" input :: Either ParseError Ast of
         (Left err) -> error (show err ++ "\n")
