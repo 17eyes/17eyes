043e1d34f2a20efeb13cfd19749ea1ccaff7eb39
Tomasz Dudziak <tomasz.dudziak@gmail.com>
2012-05-25 12:31:15 +0200

    Proper style of include/require once (#52).

--- src/Lang/Php/Ast/Analysis/Style.hs
+++ src/Lang/Php/Ast/Analysis/Style.hs
@@ -1,13 +1,14 @@
 module Lang.Php.Ast.Analysis.Style(allAnalyses) where
 
 import Data.Char(isSpace)
+import Data.Generics.Aliases(mkM)
 
 import qualified Data.Intercal as IC
 import Lang.Php.Ast
 import Lang.Php.Ast.Traversal
 import Common
 
-allAnalyses = [finishPhp]
+allAnalyses = [finishPhp, styleIncludeRequire]
 
 mkKind = IssueKind "Lang.Php.Analysis.Style"
 
@@ -36,3 +37,55 @@ finishPhp = AstAnalysis () $ \ast@(Ast _ _ sl) -> do
         issueConfidence = ICSure,
         issueContext = [] -- this issue is file-unique
     }
+
+styleIncludeRequire :: AstAnalysis
+styleIncludeRequire = AstAnalysis () $ \ast@(Ast _ _ sl) -> do
+    let stmts = map snd $ fst $ IC.breakEnd sl
+    mapM_ top_lvl_stmt stmts
+    return ast
+ where
+    top_lvl_stmt (StoredPos pos (StmtExpr (inc@(ExprInclude _ _ _ _)) _ _)) = top_lvl_inc pos inc
+    top_lvl_stmt x = traverse (mkM deep_expr) x >> return ()
+
+    deep_expr :: Expr -> TraverseState () Expr
+    deep_expr (inc@(ExprInclude _ _ _ _)) = deep_inc inc >> return inc
+    deep_expr x = return x
+
+    top_lvl_inc pos (ExprInclude Req Once _ _) = return ()
+    top_lvl_inc pos x = emitIssue $ Issue {
+        issueTitle = "top-level includes should be done with require_once",
+        issueMessage = msg_top_lvl,
+        issueFileName = Just (sourceName pos),
+        issueFunctionName = Nothing,
+        issueLineNumber = Just (sourceLine pos),
+        issueKind = mkKind "styleIncludeRequire/top_lvl",
+        issueSeverity = ISStyle,
+        issueConfidence = ICSure,
+        issueContext = [unparse x]
+    }
+
+    deep_inc (ExprInclude Inc Once _ _) = return ()
+    deep_inc x = emitIssue $ Issue {
+        issueTitle = "non-top-level includes should be done with include_once",
+        issueMessage = msg_deep,
+        issueFileName = Nothing, -- filled by emitIssue
+        issueFunctionName = Nothing,
+        issueLineNumber = Nothing, -- filled by emitIssue
+        issueKind = mkKind "styleIncludeRequire/deep",
+        issueSeverity = ISStyle,
+        issueConfidence = ICSure,
+        issueContext = [unparse x]
+    }
+
+    msg_top_lvl =
+        "Including files with require_once prevents the script from further " ++
+        "execution if the inclusion fails. It also disallows including the " ++
+        "same file multiple times. This is usually the desired behavior for " ++
+        "top-level include statements."
+
+    msg_deep =
+        "Including files with include_once disallows including the same " ++
+        "file many times but still allows the script to continue if the " ++
+        "inclusion fails. This is usually the desired behavior of include " ++
+        "statements inside functions and control structures (e.g. those " ++
+        "used to load plugins)."
