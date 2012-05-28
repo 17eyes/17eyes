43708aa28c6926822b4778a2f069283ed3ae4e76
Tomasz Dudziak <tomasz.dudziak@gmail.com>
2012-05-28 13:18:43 +0200

    Analyze gotos inside switch and namespace.

--- src/Lang/Php/Ast/Analysis/Goto.hs
+++ src/Lang/Php/Ast/Analysis/Goto.hs
@@ -21,6 +21,12 @@ gotoAnalysis = undefined
 instance EntryExtractable a => EntryExtractable (IC.Intercal WS a) where
     extract = concatMap extract . map snd . fst . IC.breakEnd
 
+instance EntryExtractable a => EntryExtractable (IC.Intercal a WS) where
+    extract = concatMap extract . map fst . fst . IC.breakEnd
+
+instance EntryExtractable a => EntryExtractable (WSCap a) where
+    extract = extract . wsCapMain
+
 instance EntryExtractable (Block Stmt) where
     extract (Block x) = extract x
 
@@ -29,6 +35,9 @@ instance EntryExtractable (StoredPos Stmt) where
     extract (StoredPos p (StmtGoto wsc _)) = [EGoto p (getLabel wsc)]
     extract (StoredPos _ (StmtBlock (Block x))) = extract x
     extract (StoredPos _ (StmtIf x)) = extract x
+    extract (StoredPos _ (StmtSwitch s)) = extract s
+    extract (StoredPos _ (StmtTry wbl catches)) = extract wbl ++ extract catches
+    extract (StoredPos _ (StmtNamespace n)) = extract n
     extract _ = []
 
 getLabel :: WSCap Label -> String
@@ -46,8 +55,23 @@ instance EntryExtractable a => EntryExtractable (Maybe a) where
     extract Nothing = []
     extract (Just x) = extract x
 
+-- non-looping control flow statments: if, switch, try..catch
+
 instance EntryExtractable IfBlock where
     extract (IfBlock _ b) = extract b
 
 instance EntryExtractable If where
     extract (If _ bs el) = extract bs ++ extract el
+
+instance EntryExtractable Switch where
+    extract (Switch _ _ _ _ cases) = do
+        (Case _ sl) <- cases
+        extract sl
+
+instance EntryExtractable Catch where
+    extract (Catch _ _ block) = extract block
+
+instance EntryExtractable Namespace where
+    extract (Namespace _ b) = extract b
+
+-- TODO: loops: dowhile for foreach, while
