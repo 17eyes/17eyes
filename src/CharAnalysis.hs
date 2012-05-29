3b37649144eaedd8ae5b61e025cff2a931b8bee8
Tomasz Dudziak <tomasz.dudziak@gmail.com>
2012-05-29 15:53:27 +0200

    Try to warn about exceeded line length (#55).

--- src/CharAnalysis.hs
+++ src/CharAnalysis.hs
@@ -5,12 +5,14 @@ module CharAnalysis(runAnalyses) where
 import Data.Char
 import qualified Data.Map as Map
 import Data.Map((!))
+import Data.List(findIndex)
 import Control.Monad
+import Data.Functor
 
 import Common
 
 runAnalyses :: FilePath -> String -> [Issue]
-runAnalyses = tabsVsSpaces
+runAnalyses fn str = tabsVsSpaces fn str ++ lineLengthCheck fn str
 
 tabsVsSpaces fn str = if emit_issue
     then [Issue {
@@ -45,3 +47,55 @@ tabsVsSpaces fn str = if emit_issue
 
     s_chosen = if use_tabs then "tabs" else "spaces"
     s_other = if use_tabs then "spaces" else "tabs"
+
+
+lineLengthCheck :: FilePath -> String -> [Issue]
+lineLengthCheck fn str = issues
+ where
+    lens = map length (lines str)
+
+    -- typical conventions on maximal line length (must be sorted)
+    magic_lens = [78, 80, 85, 90, 100, 125]
+
+    -- number of source lines that are shorter than ith limit from magic_lens
+    popularity = map (\x -> length $ filter (<= x) lens) magic_lens
+
+    -- 95% of all lines dictate the convention
+    threshold = (length lens) * 95 `div` 100
+
+    -- infer the tightest limit satisfied by <threshold> lines
+    convention :: Maybe Int
+    convention = (magic_lens !!) <$> (findIndex (>= threshold) popularity)
+
+    issues = case convention of
+        Nothing -> [Issue {
+                        issueTitle = "file contains long lines",
+                        issueMessage = "Lines in this file are very long. "
+                                    ++ "To improve readability, code line "
+                                    ++ "length is usually limited by some "
+                                    ++ "number (often 78, 80, or 100).",
+                        issueFileName = Just fn,
+                        issueFunctionName = Nothing,
+                        issueLineNumber = Nothing,
+                        issueKind = IssueKind "CharAnalysis"
+                                              "lineLengthCheck/inconsistent",
+                        issueSeverity = ISStyle,
+                        issueConfidence = ICSure,
+                        issueContext = []
+                    }]
+        Just limit -> [mkIssue limit idx | (idx,x) <- zip [0..] lens, x >= limit]
+
+    mkIssue lim idx = Issue {
+        issueTitle = "line too long",
+        issueMessage = "Almost all lines in this file are shorter than "
+                    ++ (show lim) ++ " characters, but this one is longer. "
+                    ++ "It is often desirable to maintain consistent line "
+                    ++ "length convention.",
+        issueFileName = Just fn,
+        issueFunctionName = Nothing,
+        issueLineNumber = Just (idx+1),
+        issueKind = IssueKind "CharAnalysis" "lineLengthCheck/tooLong",
+        issueSeverity = ISStyle,
+        issueConfidence = ICLikely,
+        issueContext = []
+    }
