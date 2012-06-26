185f3daef456c5d81060711bdd8068ab47905308
Mateusz Kocielski <shm@digitalsun.pl>
2012-06-26 18:53:15 +0200

    Dummy test for (#92)

---
+++ tests/parser_bugs/numlit.php
@@ -0,0 +1,15 @@
+<?php
+
+ $x = 0b0101; // since php 5.4
+ $x = 0x0000; // hex
+ $x = 0X0000; // hex
+ $x = 000001; // oct
+ $x = 1; // dec
+
+ // taken from http://php.net/manual/en/language.types.integer.php
+ $a = 1234; // decimal number
+ $a = -123; // a negative number
+ $a = 0123; // octal number (equivalent to 83 decimal)
+ $a = 0x1A; // hexadecimal number (equivalent to 26 decimal)
+
+
